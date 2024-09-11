18da21c76474ffa692600ab5f35a1f19fd4716c1
Julia Sanchis <jursanchis@gmail.com>
2024-09-11 19:24:29 +0200

    Learning ngIf-Else; ng-template

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrl: './list.component.css',
 })
 export class ListComponent {
-  public heroNames: string[] = ['Spiderman', 'Iron Man', 'Hulk', 'She-Hulk'];
+  public heroNames: string[] = [
+    'Spiderman',
+    'Iron Man',
+    'Hulk',
+    'She-Hulk',
+    'Thor',
+  ];
+
+  public deletedHero?: string; // puede ser igual a un string vacío o dejarlo como opcional con el '?'
+
+  removeLastHero(): void {
+    // this.heroNames.pop();
+    // const deletedHero = this.heroNames.pop();
+    this.deletedHero = this.heroNames.pop();
+  }
 }
